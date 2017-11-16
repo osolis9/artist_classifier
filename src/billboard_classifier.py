fe14352cd9c47ff4c63551bc71ecfcb64af3bbc8
Omar Solis <omarsolis@DN0a1f4d79.SUNet>
2017-11-15 23:17:42 -0800

    trigram, bigram, unique word ratio features

--- src/billboard_classifier.py
+++ src/billboard_classifier.py
@@ -2,23 +2,53 @@ import random
 import collections
 from util import *
 import os
+#import textblob
+import re
+#from indicoio import sentiment
+#from textblob import Textblob
 
-
+profanity = set(['fuck', 'shit', 'damn', 'bitch', 'fucked', 'nigga', 'ass', 'bastard', 'motherfucker', 'shit'])
 
 def predictor(x, weights):
 	score = 0
 	features = featureExtractor(x)
 	score = dotProduct(features, weights)
-	print score
+	#print score
 	if score <= 0:
-		return 1
-	else:
 		return -1
+	else:
+		return 1
 
 def featureExtractor(x):
 	d = collections.defaultdict(int)
-	for word in x.split():
-		d[word] = d[word] + 1
+	i = 0 
+	words = x.split()
+	uniqueWords = set([])
+	for word in words:
+		"""if i == 0:
+			sentimentScore = float(word)
+			print(sentimentScore)
+			if sentimentScore > .5:
+				d['sentiment_score'] = 1
+			continue""" #trying to get sentiment			
+		#word = re.sub("[^a-zA-Z]+", '', word).lower() #tried standardizing all words, but it made it worse
+		uniqueWords.add(word)
+		d[word] += 1 #unigram
+		
+		if i < len(words) - 1:
+			nextWord = words[i + 1]
+
+
+			#nextWord = re.sub("[^a-zA-Z]+", '', nextWord).lower() 
+			d[word + ' ' + nextWord] += 1 #bigram
+			if i < len(words) - 2:
+				d[word + ' ' + nextWord + ' ' + words[i+2]] += 1 #trigram
+	if (float(len(uniqueWords)) / float(len(words)) ) > .3: #unique word ratio
+		d['ratio_score'] = 1 
+
+		#if word in profanity:
+		#	d[word] = 1
+		#d[word] = d[word] + 1
 	return d
 
 def learnPredictor(trainExamples, numIters, eta):
@@ -34,7 +64,7 @@ def learnPredictor(trainExamples, numIters, eta):
 					lossHinge = -(v2*y)
 					weights[k2] = weights[k2] - (eta*lossHinge)
 
-	print weights
+	#print weights
 	return weights
 
 def labelTrainingExamples():
@@ -46,9 +76,12 @@ def labelTrainingExamples():
 			with open(inBillboardDirectory + '/' + filename, 'r') as f:
 				song = f.readlines()
 				songString = ' '.join(song)
-				print songString
+				#print songString
+				#sentimentScore = sentiment(songString)
+				#songString = str(sentimentScore) + ' ' + songString
 				songString = songString.replace('\n', '')
 				songString = songString.replace('\r', '')
+
 				songEntry = (songString, 1)
 				trainingExamples.append(songEntry)
 
@@ -58,46 +91,52 @@ def labelTrainingExamples():
 		with open(outOfBillboardDirectory + '/' + filename, 'r') as f:
 				song = f.readlines()
 				songString = ' '.join(song)
+				#sentimentScore = sentiment(songString)
+				#songString = str(sentimentScore) + ' ' + songString
 				songString = songString.replace('\n', '')
 				songString = songString.replace('\r', '')
-				songEntry = (songString, 1)
+				songEntry = (songString, -1)
 				trainingExamples.append(songEntry)
 	return trainingExamples
 
 def labelTestExamples():
-	inBillboardDirectory = '../lyrics/billboard-lyrics/test-set'
+	inBillboardDirectory = '../lyrics/billboard-lyrics/validation-set'
 	testExamples = []
 	for filename in os.listdir(inBillboardDirectory):
 		song = ''
 		with open(inBillboardDirectory + '/' + filename, 'r') as f:
 				song = f.readlines()
 				songString = ' '.join(song)
+				#sentimentScore = sentiment(songString)
+				#songString = str(sentimentScore) + ' ' + songString
 				songString = songString.replace('\n', '')
 				songString = songString.replace('\r', '')
 				songEntry = (songString, 1)
 				testExamples.append(songEntry)
 
-	outOfBillboardDirectory = '../lyrics/non-billboard-lyrics/test-set'
+	outOfBillboardDirectory = '../lyrics/non-billboard-lyrics/validation-set'
 	for filename in os.listdir(outOfBillboardDirectory):
 		song = ''
 		with open(outOfBillboardDirectory + '/' + filename, 'r') as f:
 				song = f.readlines()
 				songString = ' '.join(song)
+				#sentimentScore = sentiment(songString)
+				#songString = str(sentimentScore) + ' ' + songString
 				songString = songString.replace('\n', '')
 				songString = songString.replace('\r', '')
-				songEntry = (songString, 1)
+				songEntry = (songString, -1)
 				testExamples.append(songEntry)
 	return testExamples
 
 def main():
 	trainingExamples = labelTrainingExamples()
-	print len(trainingExamples)
-	numIters = 1
+	numIters = 100
 	eta = .01
+
 	weights = learnPredictor(trainingExamples, numIters, eta)
-	print weights
+
 	testExamples = labelTestExamples()
-	print len(testExamples)
+
 	totalTested = len(testExamples)
 	correct = 0
 	for testExample in testExamples:
@@ -106,11 +145,8 @@ def main():
 		if (actual == prediction):
 			correct += 1
 	percentageCorrect = float(correct) / float(totalTested)
-
 	print "Ratio Classified Correctly: " + str(percentageCorrect)
-	#read all trainingexamples and label with a 1 or -1 and put in a list
-	#call learn predictor on training examples
-	#see how well it did
+
 
 if __name__ == "__main__":
 	main()
